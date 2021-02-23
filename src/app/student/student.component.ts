f65d3502844c7659d220f71101797a8c1bd9e576
EslamKhder <eslamkhder81@gmail.com>
2021-02-23 08:59:36 +0200

    Component #2

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student',
+  //selector: '[app-student]',
+  //template: '<h1>Eslam</h1> <app-person></app-person>',
   templateUrl: './student.component.html',
+  /*styles: [`
+    p{
+      color: yellow;
+    }
+  `]*/
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
