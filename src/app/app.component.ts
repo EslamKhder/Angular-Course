cd7b304e6e368eb36e8770a0295c8a6d6294f9e5
EslamKhder <eslamkhder81@gmail.com>
2021-03-15 19:36:51 +0200

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,17 @@ export class AppComponent {
 
   myText: string = "";
   friends: string[] = ["Ahmed","Karim","Yaser","Natore","Eslam"];
+  mycolorBackGround: string = "black";
+  myColor: String = "white";
 
 
+  doRedYellow() {
+    this.mycolorBackGround = "red";
+    this.myColor = "yellow";
+  }
+
+  doBrownWhite() {
+    this.mycolorBackGround = "brown";
+    this.myColor = "white";
+  }
 }
